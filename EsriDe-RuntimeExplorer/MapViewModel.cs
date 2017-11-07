3aff3996401c59bcdfb1b192e2ebc1a60a591445
Jens Buchta <j.buchta@esri.de>
2017-11-07 11:15:46 +0100

    display basic Map

---
+++ EsriDe-RuntimeExplorer/MapViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Esri.ArcGISRuntime.Mapping;
+
+namespace EsriDe_RuntimeExplorer
+{
+    public class MapViewModel : DependencyObject
+    {
+        public Map Map { get; } = new Map(Basemap.CreateStreetsVector());
+    }
+}
