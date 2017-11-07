459b9b8c9523d1d5acac974928c8f1753502e154
Jens Buchta <j.buchta@esri.de>
2017-11-07 14:05:37 +0100

    reverse order of layers in LegendControl

---
+++ EsriDe-RuntimeExplorer/ReverseItemsConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EsriDe.RuntimeExplorer
+{
+    public class ReverseItemsConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IEnumerable<object>)
+            {
+                var list = new List<object>();
+                list.AddRange((IEnumerable<object>)value);
+                list.Reverse();
+                return list;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
