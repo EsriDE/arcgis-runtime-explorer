967e96325b61d954469074700007774a9bbc7399
Maximilian Glas <m.glas@esri.de>
2017-11-07 17:41:06 +0100

    Mapwechsel beobachten

--- EsriDe-RuntimeExplorer/ViewModel/LayerDetailViewModel.cs
+++ EsriDe-RuntimeExplorer/ViewModel/LayerDetailViewModel.cs
@@ -6,13 +6,31 @@ namespace EsriDe.RuntimeExplorer.ViewModel
 {
 	public class LayerDetailViewModel : ViewModelBase
 	{
-		public Map Map { get; }
+		public Map Map { get; set; }
 
 		public LayerDetailViewModel()
 		{
 			var instance = SimpleIoc.Default.GetInstance<MainDataViewModel>();
 			Map = instance.SelectedMapView.Map;
+
+			//der Wechsel eines Tabs interessiert mich
+			instance.PropertyChanged += (sender, args) =>
+			{
+				if (args.PropertyName == nameof(MainDataViewModel.SelectedMapView))
+				{
+					Map = instance.SelectedMapView.Map;
+					//der Wechsel einer Map innerhalb eines Tabs interessiert mich auch
+					instance.SelectedMapView.PropertyChanged += (sender1, args1) =>
+					{
+						if (args1.PropertyName == nameof(MapViewModel.Map))
+						{
+							Map = instance.SelectedMapView.Map;
+						}
+					};
+				}
+			};
 		}
+		
 
 		public LayerCollection Layers
 		{
