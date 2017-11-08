094081fd2f177bb7d0de342f241b628417c0b29f
Jens Buchta <j.buchta@esri.de>
2017-11-08 09:08:39 +0100

    add MapContentControl

---
+++ EsriDe-RuntimeExplorer/Controls/MapContentControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Esri.ArcGISRuntime.UI;
+
+namespace EsriDe.RuntimeExplorer.Controls
+{
+    /// <summary>
+    /// Interaction logic for MapContentControl.xaml
+    /// </summary>
+    public partial class MapContentControl : UserControl
+    {
+        public MapContentControl()
+        {
+            InitializeComponent();
+        }
+
+        private void GeoView_OnDrawStatusChanged(object sender, DrawStatusChangedEventArgs e)
+        {
+            
+        }
+    }
+}
