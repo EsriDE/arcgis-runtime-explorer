10d05b8c1c1a86e2f30aa046856f663ff8be5c27
Maximilian Glas <m.glas@esri.de>
2017-11-21 17:15:45 +0100

    new Bookmark select concept (work in progress, not ready yet)

---
+++ EsriDe-RuntimeExplorer/Converter/CountToVisibiltyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EsriDe.RuntimeExplorer.Converter
+{
+	public class CountToVisibiltyConverter : BaseConverter, IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				return System.Convert.ToInt32(value) > 0 ? Visibility.Visible : Visibility.Hidden;
+			}
+			catch (Exception)
+			{
+				return value;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
