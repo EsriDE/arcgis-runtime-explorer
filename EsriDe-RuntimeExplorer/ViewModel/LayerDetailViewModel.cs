7f54ff62523b4ad16ebeeb76bfb8e15987f7736a
jebu <j.buchta@esri.de>
2017-11-22 11:51:16 +0100

    add csv export

--- EsriDe-RuntimeExplorer/ViewModel/LayerDetailViewModel.cs
+++ EsriDe-RuntimeExplorer/ViewModel/LayerDetailViewModel.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Windows.Input;
+using CsvHelper;
 using Esri.ArcGISRuntime;
 using Esri.ArcGISRuntime.Mapping;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Win32;
 
 namespace EsriDe.RuntimeExplorer.ViewModel
 {
 	public class LayerDetailViewModel : ViewModelBase
 	{
-		public Map Map { get; set; }
+	    public Map Map { get; set; }
 
 		public LayerDetailViewModel()
 		{
 			var instance = SimpleIoc.Default.GetInstance<MainDataViewModel>();
 			Map = instance.SelectedMapView.Map;
+
+            ExportToCsvCommand = new RelayCommand(() =>
+            {
+                var dlg = new SaveFileDialog {Filter = "Comma Separated Values|*.csv"};
+                if (dlg.ShowDialog() == true)
+                {
+                    using (var sw = new StreamWriter(dlg.FileName))
+                    using (var csv = new CsvWriter(sw))
+                    {
+                        csv.WriteRecords(FeatureLayers);
+                    }
+                }
+            });
 
 			//der Wechsel eines Tabs interessiert mich
 			instance.PropertyChanged += (sender, args) =>
@@ -33,16 +51,11 @@ namespace EsriDe.RuntimeExplorer.ViewModel
 				}
 			};
 		}
-		
 
-		public IEnumerable<FeatureLayer> FeatureLayers
-		{
-			get { return Map.OperationalLayers.OfType<FeatureLayer>(); }
-		}
+	    public IEnumerable<FeatureLayer> FeatureLayers => Map.OperationalLayers.OfType<FeatureLayer>();
 
-		public IEnumerable<RasterLayer> RasterLayers
-		{
-			get { return Map.OperationalLayers.OfType<RasterLayer>(); }
-		}
+	    public IEnumerable<RasterLayer> RasterLayers => Map.OperationalLayers.OfType<RasterLayer>();
+
+	    public ICommand ExportToCsvCommand { get; private set; }
 	}
 }
