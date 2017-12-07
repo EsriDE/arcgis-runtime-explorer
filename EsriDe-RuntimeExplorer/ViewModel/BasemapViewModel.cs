ff86478bbc201e2406e91fce01d5a4e263afff45
Jens Buchta <j.buchta@esri.de>
2017-12-07 13:11:15 +0100

    add basemap from tpk

---
+++ EsriDe-RuntimeExplorer/ViewModel/BasemapViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+
+namespace EsriDe.RuntimeExplorer.ViewModel
+{
+    public class BasemapViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        public ICommand ApplyBasemapCommand { get; private set; }
+    }
+}
