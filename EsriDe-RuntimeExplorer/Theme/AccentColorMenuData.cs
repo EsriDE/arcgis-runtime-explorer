cc5067a108c4e692dd99ef5b4f3bc64accbe7766
Maximilian Glas <m.glas@esri.de>
2017-12-07 15:20:24 +0100

    Theme change

---
+++ EsriDe-RuntimeExplorer/Theme/AccentColorMenuData.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using MahApps.Metro;
+
+namespace EsriDe.RuntimeExplorer.Theme
+{
+    public class AccentColorMenuData
+    {
+        public string Name { get; set; }
+        public Brush BorderColorBrush { get; set; }
+        public Brush ColorBrush { get; set; }
+
+        private ICommand _changeAccentCommand;
+
+        public ICommand ChangeAccentCommand
+        {
+            get { return this._changeAccentCommand ?? (_changeAccentCommand = new SimpleCommand { CanExecuteDelegate = x => true, ExecuteDelegate = x => this.DoChangeTheme(x) }); }
+        }
+
+        protected virtual void DoChangeTheme(object sender)
+        {
+            var theme = ThemeManager.DetectAppStyle(Application.Current);
+            var accent = ThemeManager.GetAccent(this.Name);
+            ThemeManager.ChangeAppStyle(Application.Current, accent, theme.Item1);
+        }
+    }
+
+}
