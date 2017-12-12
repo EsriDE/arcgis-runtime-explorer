71cb1c9f897faf267feff3ecb2b87af14c35f7d4
Maximilian Glas <m.glas@esri.de>
2017-12-12 17:57:43 +0100

    reformatting

--- EsriDe-RuntimeExplorer/LayerDetailWindow.xaml.cs
+++ EsriDe-RuntimeExplorer/LayerDetailWindow.xaml.cs
@@ -4,42 +4,42 @@ using MahApps.Metro.Controls;
 
 namespace EsriDe.RuntimeExplorer
 {
-	/// <summary>
-	/// Interaction logic for LayerDetailWindow.xaml
-	/// </summary>
-	public partial class LayerDetailWindow : MetroWindow
-	{
-		public LayerDetailWindow()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Interaction logic for LayerDetailWindow.xaml
+    /// </summary>
+    public partial class LayerDetailWindow : MetroWindow
+    {
+        public LayerDetailWindow()
+        {
+            InitializeComponent();
 
-			FeatureLayerGrid.AutoGeneratingColumn += FeatureLayerGrid_AutoGeneratingColumn;
-		    this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 0.9);
-		    this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 0.9);
+            FeatureLayerGrid.AutoGeneratingColumn += FeatureLayerGrid_AutoGeneratingColumn;
+            this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 0.9);
+            this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 0.9);
         }
 
-		private void FeatureLayerGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
-		{
-			if (e.PropertyName == "SpatialReference")
-			{
-				var templateColumn = new DataGridTemplateColumn
-				{
-					Header = e.PropertyName,
-					CellTemplate = (DataTemplate) Resources["SpatRefTemplate"],
-					SortMemberPath = e.PropertyName
-				};
-				e.Column = templateColumn;
-			}
-			if (e.PropertyName == "SelectionColor")
-			{
-				var templateColumn = new DataGridTemplateColumn
-				{
-					Header = e.PropertyName,
-					CellTemplate = (DataTemplate)Resources["ColorTemplate"],
-					SortMemberPath = e.PropertyName
-				};
-				e.Column = templateColumn;
-			}
-		}
-	}
+        private void FeatureLayerGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "SpatialReference")
+            {
+                var templateColumn = new DataGridTemplateColumn
+                {
+                    Header = e.PropertyName,
+                    CellTemplate = (DataTemplate) Resources["SpatRefTemplate"],
+                    SortMemberPath = e.PropertyName
+                };
+                e.Column = templateColumn;
+            }
+            if (e.PropertyName == "SelectionColor")
+            {
+                var templateColumn = new DataGridTemplateColumn
+                {
+                    Header = e.PropertyName,
+                    CellTemplate = (DataTemplate) Resources["ColorTemplate"],
+                    SortMemberPath = e.PropertyName
+                };
+                e.Column = templateColumn;
+            }
+        }
+    }
 }
