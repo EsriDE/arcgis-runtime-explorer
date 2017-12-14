80c6ccc891e874739d023df47f1a0775462a3065
Maximilian Glas <m.glas@esri.de>
2017-12-14 16:22:27 +0100

    ensure that license was read at least once

--- EsriDe-RuntimeExplorer/MainWindow.xaml.cs
+++ EsriDe-RuntimeExplorer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using EsriDe.RuntimeExplorer.Properties;
 using MahApps.Metro.Controls;
 
 namespace EsriDe.RuntimeExplorer
@@ -14,6 +15,18 @@ namespace EsriDe.RuntimeExplorer
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        //ToDo: not so pretty her - has to be in ViewModel
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!Settings.Default.UserLiscenseHasSeenOnce)
+            {
+                ShowAbout(null, null);
+                Settings.Default.UserLiscenseHasSeenOnce = true;
+                Settings.Default.Save();
+            }
         }
 
         private void FilePath_Changed(object sender, System.Windows.Data.DataTransferEventArgs e)
--- EsriDe-RuntimeExplorer/Properties/Settings.Designer.cs
+++ EsriDe-RuntimeExplorer/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace EsriDe.RuntimeExplorer.Properties {
                 this["UserAccent"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool UserLiscenseHasSeenOnce {
+            get {
+                return ((bool)(this["UserLiscenseHasSeenOnce"]));
+            }
+            set {
+                this["UserLiscenseHasSeenOnce"] = value;
+            }
+        }
     }
 }
