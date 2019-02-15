b97e233dfc18a30bdae51b9d7e5620bde8756132
Maximilian Glas <m.glas@esri.de>
2019-02-15 14:01:11 +0100

    Compass and reference scale implemented, Page Up/Down Key let the map rotate, Click on Compass resets rotation to north

--- EsriDe-RuntimeExplorer/Controls/MapContentControl.xaml.cs
+++ EsriDe-RuntimeExplorer/Controls/MapContentControl.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using CommonServiceLocator;
+using Esri.ArcGISRuntime.Toolkit.UI.Controls;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.UI.Controls;
 using EsriDe.RuntimeExplorer.ViewModel;
@@ -45,6 +47,29 @@ namespace EsriDe.RuntimeExplorer.Controls
             if (mapViewModel != null)
             {
                 mapViewModel.MapView = MapView;
+            }
+        }
+
+        private void Compass_OnTouchDown(object sender, TouchEventArgs e)
+        {
+            if (sender is Compass compass)
+            {
+                compass.Heading = 0;
+            }
+        }
+
+        private async void MapView_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var mapView = (MapView)sender;
+            
+            if (e.Key == Key.PageDown && !mapView.IsNavigating)
+            {
+                await mapView.SetViewpointRotationAsync(mapView.MapRotation + 10);
+            }
+
+            if (e.Key == Key.PageUp && !mapView.IsNavigating)
+            {
+                await mapView.SetViewpointRotationAsync(mapView.MapRotation - 10);
             }
         }
     }
